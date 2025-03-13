234dd0cc293dcecdadaef3ee6880f3882ffd21f8
Ubuntu Desktop Kels <alcorizamichael@gmail.com>
2025-03-13 17:27:15 +0800

    Add videov list in the carousel container
    Video Tutorial: 1:33:08

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -6,9 +6,20 @@ const VideoCarousel = () => {
     <>
         <div className='flex items-center'>
             {hightlightsSlides.map((list, i) => (
-                <div key={list.id} id='slider'>
+                <div key={list.id} id='slider' className='sm:pr-20 pr-10'>
 
                     <div className='video-carousel_container'>
+
+                        <div className='w-full h-full flex-center rounded-3xl overflow-hidden bg-black'>
+                            <video
+                                id='video'
+                                playsInline={true}
+                                preload='auto'
+                                muted
+                            >
+                                <source src={list.video} type='video/mp4' />
+                            </video>
+                        </div>
 
                     </div>
 
